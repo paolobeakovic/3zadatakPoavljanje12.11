9d356d1954dd497190a05ef240e5a16a2436f3a2
Paolo <55134717+paolobeakovic@users.noreply.github.com>
2020-11-17 10:28:29 +0100

    Add files via upload

---
+++ beakovicPaolo3/beakovicPaolo3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beakovicPaolo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] niz = new string[3];
+            Console.WriteLine("Upiši 1. string");
+            niz[0] = Console.ReadLine();
+
+            Console.WriteLine("Upiši 2. string");
+            niz[1]= Console.ReadLine();
+
+            Console.WriteLine("Upiši 3. string");
+            niz[2] = Console.ReadLine();
+
+            Array.Sort(niz);
+            Array.Reverse(niz);
+
+            Console.WriteLine("Razvrsteni stringovi : ");
+            for (int i=0; i<3; i++)
+            {
+                Console.WriteLine(niz[1]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
